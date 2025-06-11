ac7015495cd2990986eae632536d0c78e6bc1a11
adrianjosephconsulting <tequilacanaria@hotmail.it>
2025-06-11 19:37:19 -0400

    removed venv

--- venv/lib/python3.8/site-packages/garminexport/logging_config.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-LOG_LEVELS = {
-    "DEBUG": logging.DEBUG,
-    "INFO": logging.INFO,
-    "WARNING": logging.WARNING,
-    "ERROR": logging.ERROR
-}
-"""Command-line (string-based) log-level mapping to logging module levels."""
